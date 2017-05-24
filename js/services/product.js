ac948703b8da02216df474166dd6dec39574392a
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-05-24 14:04:00 -0400

    2:03 commit

--- js/services/product.js
+++ js/services/product.js
@@ -21,25 +21,4 @@ module.exports = {
     }, 
 }
 
-module.export = {
-    name: 'ProductService',
-    func: function ($http) {
-
-        const searchResults = [];
-
-        return {
-            addSearchResults(searchString) {
-
-                $http.get('https://tiy-28202.herokuapp.com/shop/search?q=' + searchString).then(function (response) {
-                    // populate 'searchResults' using the results from the get request
-                });
-            },
-
-            getSearchResults() {
-                return searchResults;
-            }
-        };
-    },
-};
-
 
