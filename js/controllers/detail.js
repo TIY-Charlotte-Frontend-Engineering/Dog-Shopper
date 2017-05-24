8c44d96c08eac7c48acd51f2ebac45ebceffa432
Neilson Gamble <neilgamb@Neilsons-MacBook-Pro.local>
2017-05-24 17:12:03 -0400

    File names cleaned up

--- js/controllers/detail.js
+++ js/controllers/detail.js
@@ -1,18 +1,5 @@
 module.exports = {
     name: 'ProductDetailController',
-    func: function ($scope, ProductService) { // may need $stateParams, not sure yet
-        // $scope.searchResults = ProductService.getSearchResults()
-
-        ProductService.getAllItems().then(function (results) {
-            let popItems = [];
-
-            for (let i = 0; i < results.length; i++) {
-                if (results[i].popular === true) {
-                    popItems.push(results[i]);
-
-                };
-            };
-            $scope.popItems = popItems;
-        })
+    func: function ($scope, ProductService) { 
     },
 }
