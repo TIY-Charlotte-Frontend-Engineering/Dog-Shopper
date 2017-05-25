d8d958e4ec1d7c632c95da6e7786e30019df68f7
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-05-25 12:06:20 -0400

    Wednesday night stuff

--- js/controllers/list.js
+++ js/controllers/list.js
@@ -1,7 +1,14 @@
 module.exports = {
     name: 'ProductListController',
     func: function ($scope, ProductService) { // may need $stateParams, not sure yet
-        
+
+        ProductService.getAllItems().then(function (results) {
+            let allItems = [];
+            for (let i = 0; i < results.length; i++) {
+                allItems.push(results[i]);
+            }
+            $scope.allItems = allItems;
+        });
     },
 }
 
